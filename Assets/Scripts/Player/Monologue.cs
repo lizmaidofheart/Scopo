8ab362130d1f063c3f4420e1fa9e96fd7f3e7147
Liz <elizabeth.farrington.miller@gmail.com>
2023-03-07 15:35:34 +0000

    monologue doesnt need to be a singleton

--- Assets/Scripts/Player/Monologue.cs
+++ Assets/Scripts/Player/Monologue.cs
@@ -5,26 +5,6 @@ using UnityEngine;
 
 public class Monologue : MonoBehaviour
 {
-    // this is a singleton acting as an easy reference for the monologue text
-    // it being a singleton means its accessible from anywhere without complex getcomponents etc
-
-    private static Monologue _instance;
-    public static Monologue Instance { get { return _instance; } }
-
-    private void Awake()
-    {
-        // singleton setup
-
-        if (_instance != null && _instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            _instance = this;
-        }
-    }
-
     // this script manages the monologue text at the top of the screen
 
     [Header("Text Input")]
