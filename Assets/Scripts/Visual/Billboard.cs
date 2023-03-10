6ee3da941d484f9a8e15b497008d8755d58a7270
Liz <elizabeth.farrington.miller@gmail.com>
2023-03-10 11:45:02 +0000

    tuft billboards done and ready for scattering

--- Assets/Scripts/Visual/Billboard.cs
+++ Assets/Scripts/Visual/Billboard.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    private Transform cameraTransform;
+
+    private void Start()
+    {
+        cameraTransform = PlayerReference.Instance.cam.transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookPos = cameraTransform.position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
     }
 }
