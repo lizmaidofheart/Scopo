5b9dd27f175006d0f8e679d5155e4392bc93d6ea
Liz <elizabeth.farrington.miller@gmail.com>
2023-03-13 17:51:57 +0000

    fixed anglematch

--- Assets/Scripts/Misc/LerpToAngleMatch.cs
+++ Assets/Scripts/Misc/LerpToAngleMatch.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LerpToAngleMatch : MonoBehaviour
 {
+    // this component gets the associated object to lerp to match the rotation of a target transform.
+    // it can either match on all axis, or just the y axis (the yaw) while dampening the match on the other axis
+
     [SerializeField] Transform match;
     [SerializeField] float slerpRate = 0.1f;
     [SerializeField] float slerpDampener = 0.5f;
@@ -26,7 +29,7 @@ public class LerpToAngleMatch : MonoBehaviour
     {
         if (isActive)
         {
-            Quaternion slerped = Quaternion.Slerp(myTransform.rotation, match.rotation * angleOffset, slerpRate);
+            Quaternion slerped = Quaternion.Slerp(myTransform.rotation, match.rotation * angleOffset, slerpRate * Time.deltaTime);
 
             if (matchYOnly) // mostly only move on the y axis
             {
@@ -38,17 +41,18 @@ public class LerpToAngleMatch : MonoBehaviour
                 Quaternion modifiedAngleToDampenTo = Quaternion.Euler(eulerToDampenTo.x, eulerToDampenTo.y, eulerToDampenTo.z);
 
                 Quaternion dampenedMatch = Quaternion.Lerp(modifiedAngleToDampenTo * dampenAngleOffset, match.rotation * angleOffset, slerpDampener);
-                Quaternion reslerped = Quaternion.Slerp(myTransform.rotation, dampenedMatch, slerpRate);
+                Quaternion reslerped = Quaternion.Slerp(myTransform.rotation, dampenedMatch, 0.2f);
 
                 Vector3 modifiedRotation = myTransform.eulerAngles;
                 modifiedRotation.y = slerped.eulerAngles.y;
+
                 modifiedRotation.x = reslerped.eulerAngles.x;
                 modifiedRotation.z = reslerped.eulerAngles.z;
+
                 myTransform.eulerAngles = modifiedRotation;
             }
             else // move on all axis
             {
-
                 myTransform.rotation = slerped;
             }
         }
