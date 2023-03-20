9f03934513937bc3c5aef75c64805f362f21928a
Liz <elizabeth.farrington.miller@gmail.com>
2023-03-20 14:16:38 +0000

    more studio progress, promptinarea script

--- Assets/Scripts/UI/Leave.cs
+++ Assets/Scripts/UI/Leave.cs
@@ -4,54 +4,13 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class Leave : MonoBehaviour
+public class Leave : PromptInArea
 {
-    [SerializeField] TextMeshProUGUI prompt;
-    [SerializeField] float maxRadius = 5;
-    [SerializeField] float minRadius = 3;
-    Transform playerTransform;
-    [SerializeField] KeyCode leaveButton;
-    [SerializeField] int sceneIndex = 1;
+    [SerializeField] int sceneIndex = 2;
 
-    // Start is called before the first frame update
-    void Start()
+    public override void Action()
     {
-        Color textColour = prompt.color;
-        textColour.a = 0;
-        prompt.color = textColour;
-
-        playerTransform = PlayerReference.Instance.transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // get opacity value based on player proximity to point (max distance to show = maxRadius)
-        float distanceToPlayer = (playerTransform.position - transform.position).magnitude;
-        float opacity;
-        if (distanceToPlayer <= minRadius) opacity = 1;
-        else if (distanceToPlayer > maxRadius) opacity = 0;
-        else opacity = 1 - (distanceToPlayer - minRadius) / (maxRadius - minRadius);
-
-        // change opacity of text based on player proximity to point
-        Color textColour = prompt.color;
-        textColour.a = opacity;
-        prompt.color = textColour;
-
-        // if player is close enough to leave, let do it on button press
-        if (Input.GetKeyDown(leaveButton))
-        {
-            SceneManager.LoadScene(sceneIndex);
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // Draw spheres for radii
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, minRadius);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, maxRadius);
+        base.Action();
+        SceneManager.LoadScene(sceneIndex);
     }
 }
